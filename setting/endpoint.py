24c7bff291a4bf82f141ddb8c3d4d32a315992b3
DaffaPintu <daffa@pintu.co.id>
2024-05-24 19:31:50 +0700

    FIX git guardian

--- setting/endpoint.py
+++ setting/endpoint.py
@@ -1,4 +1,6 @@
 # ======================================== HOST ========================================
+import os
+
 host_gorest = "https://gorest.co.in/public/v2"
 host_qase_io = "https://api.qase.io/v1"
 
@@ -8,9 +10,9 @@ api_user_wrong = host_gorest + "/userssss"
 api_result_qase_io = host_qase_io + "/result"
 
 # ======================================== CONFIG ========================================
-TOKEN_QASE_IO = "11f2ce05c3aa19969db2065ec4911a4f7436222c99d6f098fc40330f44927ff7"
+TOKEN_QASE_IO = os.environ.get('QASE_IO_TOKEN')
 PROJECT_CODE_QASE_IO = "DPQ"
 TEST_RUN_QASE_IO = "1"
 
 # ======================================== SLACK ========================================
-WEBHOOK = "https://hooks.slack.com/services/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"
+WEBHOOK = os.environ.get('WEBHOOK_SLACK')
